08ce429464a99dbe11981fd5616987a7a45f5c10
Valérian Wislez <valerian@valdev.leomigaland.be>
2025-05-03 20:57:20 +0200

    Changed to 0xabc hexadecimal notation.

--- README.md
+++ README.md
@@ -2,4 +2,7 @@
 An attempt at writting an operating system. This will take a long time and much effort, but like many before me, I'm gonna do it. 
 
 ## This repository
-It will serve mostly for organizing my research first. I might include some directories for running a basic "Hello, world!" inside an emulator from a bottable partition in the future. 
+It will serve mostly for organizing my research first. I might include some directories for running a basic "Hello, world!" inside an emulator from a bootable partition in the future. 
+
+## Structure
+See references/ to have a look at all the references I used to work on this project. See journal/ to read my adventures (in French). See experiments to have a look at all the things I'm trying to do. 
--- experiments/femto/femto.asm
+++ experiments/femto/femto.asm
@@ -1,37 +1,35 @@
-BITS 16             ; directive telling NASM to work in 16 bits mode
+BITS 16                 ; directive telling NASM to work in 16 bits mode
 
 start:
-	mov ax, 07C0h		; Set up 4K stack space after this bootloader
-	add ax, 288		; (4096 + 512) / 16 bytes per paragraph
+	mov ax, 0x7C0		; Set up 4K stack space after this bootloader
+	add ax, 288		    ; (4096 + 512) / 16 bytes per paragraph
 	mov ss, ax
 	mov sp, 4096
 
-	mov ax, 07C0h		; Set data segment to where we're loaded
+	mov ax, 0x7C0		; Set data segment to where we're loaded
 	mov ds, ax
 
 
 	mov si, text_string	; Put string position into SI
 	call print_string	; Call our string-printing routine
 
-	jmp $			; Jump here - infinite loop!
+	jmp $			    ; Jump here - infinite loop!
 
-
-	text_string db 'Welcome to femtOS!', 0
-
+	text_string 
+        db 'Welcome to femtOS!', 0  ; null-terminated
 
 print_string:			; Routine: output string in SI to screen
-	mov ah, 0Eh		; int 10h 'print char' function
+	mov ah, 0xE		    ; int 10h 'print char' function
 
 .repeat:
-	lodsb			; Get character from string
+	lodsb			    ; Get character from string
 	cmp al, 0
-	je .done		; If char is zero, end of string
-	int 10h			; Otherwise, print it
+	je .done		    ; If char is zero, end of string
+	int 0x10			; Otherwise, print it
 	jmp .repeat
 
 .done:
 	ret
 
-
 	times 510-($-$$) db 0	; Pad remainder of boot sector with 0s
-	dw 0xAA55		; The standard PC boot signature
+	dw 0xAA55		    ; The standard PC boot signature
