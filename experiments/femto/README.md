d9f20b6dfe863a24c8812911f419069d829e3f02
Valérian Wislez <valerian@valdev.leomigaland.be>
2025-05-07 17:16:46 +0200

    Removed the old MikeOS floppy and changed the script to create a floppy disk sized image.

---
+++ experiments/femto/README.md
@@ -0,0 +1,9 @@
+# Usage
+You need nasm and qemu to use the run.sh script.
+
+On a Debian-like system:
+
+```
+sudo apt install nasm qemu-system-x86
+```
+
--- experiments/femto/run.sh
+++ experiments/femto/run.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-# Compile in binary mode using NASM :
+# Compile the bootloader in binary mode using NASM
 nasm -f bin -o femto.bin femto.asm
 
-# Rewrite the MikeOS floppy image with the content of femto.bin
-dd status=noxfer conv=notrunc if=femto.bin of=femto.flp
+# Create a floppy disk (2880 * 512 bytes) filled with zeros
+dd if=/dev/zero of=femto.flp bs=512 count=2880
 
+# Rewriting the 512 first bytes with the bootloader
+dd conv=notrunc if=femto.bin of=femto.flp
+
+# Launching it into qemu
 qemu-system-i386 -fda femto.flp
